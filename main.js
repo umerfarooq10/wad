3dea2b09dfefe93f88f0ca3563d2f80509528c01
Umer farooq <44427636+umerfarooq10@users.noreply.github.com>
2018-12-01 23:06:03 +0500

    Add files via upload
    class activity with description only..

---
+++ main.js
@@ -0,0 +1,197 @@
+//============================================
+//-----------  STEP - 1 ----------------------
+// we can add our task using step1. in function ADD we can add our tasks..!!
+
+
+var todos = [];
+function add() {
+    var task = document.getElementById("task").value;
+    todos.push(task);
+    document.getElementById('todos').innerText = todos;
+}
+
+
+
+
+//============================================
+//-----------  STEP - 2 ----------------------
+//When we click on add button the text we entered in list input disappears
+// and displayed in todoid ,if i enter text again and click add button then my previous and current entry
+// is displayed.
+
+var todos = [];
+function add() {
+    var task = document.getElementById('task').value;
+    todos.push(task);
+    document.getElementById('task').value = '';
+    show();
+}
+
+function show() {
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' + todos[i] + '</li>';
+        li.classList.add("list-group-item");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+}
+
+
+//============================================
+//-----------  STEP - 3 ----------------------
+// Delete button comes to delete the data we entered but it is not deleting the data.
+// If i enter text again and click add button then only my current entry
+// is display along with a delete button.
+
+
+
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' + todos[i].task + '</li>' +
+            '<button class="btn btn-danger">' +
+            '<i class="fas fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>';
+        li.classList.add("list-group-item");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+}
+show();
+
+
+
+
+//============================================
+//-----------  STEP - 4 ----------------------
+//One difference in Step 4 is that the delete button is working perfectly..!
+
+
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' + todos[i].task + '</li>' +
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>';
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+    }
+}
+
+function done(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
+
+var list = document.querySelector('value');
+list.addEventListener('click', function(e) {
+    if(todos[e.target.id].isDone) {
+        e.target.classList.toggle('checked');
+    }
+}, false);
+
+
+
+//============================================
+//-----------  STEP - 5 ----------------------
+//============================================
+
+/*
+
+CLASS ACTIVITY (HOME WORK) :
+
+1- Enable State of todos by clicking on the text completed, started etc
+HINT: use text-decoration:line-through; property of CSS
+
+2- Enable Editing todos in text field to update text
+
+
+
+*/
+
