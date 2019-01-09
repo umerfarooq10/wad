e60d36423c44f0dc8f6216f58171d378386d583a
Umer farooq <44427636+umerfarooq10@users.noreply.github.com>
2019-01-09 18:21:36 +0500

    Add files via upload
    Quiz 5

---
+++ insert_brand.php
@@ -0,0 +1,54 @@
+<?php
+require_once "db_connection.php";
+if(isset($_POST['insert_pro'])){
+    //getting text data from the fields
+
+    $pro_cat = $_POST['pro_title'];
+
+    $insert_product = "insert into brands (brand_title) 
+                  VALUES ('$pro_cat')";
+    $insert_pro = mysqli_query($con, $insert_product);
+    if($insert_pro){
+        header("location: ".$_SERVER['PHP_SELF']);
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Insert Product</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <style>
+        * {
+            font-family: 'Old Standard TT', serif;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1 class="text-center my-4"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span> BRANDS </h1>
+    <form action="insert_brand.php" method="post" enctype="multipart/form-data">
+        <div class="row">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <div class="input-group-text"><i class="fas fa-file-signature"></i></div>
+                </div>
+                <input type="text" class="form-control" id="pro_title" name="pro_title" placeholder="Enter Brand Title" >
+            </div>
+        </div>
+</div>
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto"></div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <button type="submit" name="insert_pro" class="btn btn-primary btn-block"><i class="fas fa-plus"></i> Insert Now </button>
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>
+
